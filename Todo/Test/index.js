ae94dde1f50768ee6528345d61a77404b6856ff4
Simon K <simon.kochanek@me.com>
2020-07-14 15:12:13 +0200

    continuing

--- Todo/Test/index.js
+++ Todo/Test/index.js
@@ -2,10 +2,13 @@
 new Vue({
   el: '#TestVue',
   data: {
-    parentMessage: 'Parent',
-    items: [
-      { message: 'Foo' },
-      { message: 'Bar' }
-    ]
+  sets: [[ 1, 2, 3, 4, 5 ], [6, 7, 8, 9, 10]]
+},
+methods: {
+  even: function (numbers) {
+    return numbers.filter(function (number) {
+      return number % 2 === 0
+    })
   }
+}
 })
